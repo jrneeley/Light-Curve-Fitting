a7dc2142afd9115f7041cec249551d3052f5400e
jrneeley <jillian.neeley@gmail.com>
2021-01-22 16:15:38 -0500

    syncing with henrietta

--- lcv_fitting.py
+++ lcv_fitting.py
@@ -3,14 +3,39 @@ import re
 import matplotlib.pyplot as mp
 from astropy.stats import LombScargle
 from scipy import stats
-import peakutils
+#import peakutils
 from astropy.stats import sigma_clip
-from IPython import display
+import time
+
+def read_optical_lcv(lcv_file, old=0):
+
+    dtype1 = np.dtype([ ('mag', float), ('err', float), ('filter', int),
+        ('year', int), ('day', float), ('source', (np.unicode_, 35)) ])
+
+    if old == 1:
+        data = np.loadtxt(lcv_file, dtype=dtype1, usecols=(0,1,2,4,5,8))
+    if old == 0:
+        data = np.loadtxt(lcv_file, dtype=dtype1, usecols=(0,1,2,4,5,10))
+
+    filt_name = np.zeros(len(data['filter']), dtype=(np.unicode_, 2))
+    filt_name[data['filter'] == 1] = 'V'
+    filt_name[data['filter'] == 2] = 'B'
+    filt_name[data['filter'] == 3] = 'I'
+    filt_name[data['filter'] == 4] = 'R'
+    filt_name[data['filter'] == 5] = 'U'
+    mjd = data['year']*1000. + data['day'] - 2400000.5
+
+
+    new_dt = np.dtype([('filter', (np.unicode_, 2)), ('mjd', float), ('mag', float),
+        ('err', float), ('source', (np.unicode_, 35))])
+    new_array = np.array(list(zip(filt_name, mjd, data['mag'], data['err'], data['source'])), dtype=new_dt)
+
+    return new_array
 
 # reads in light curve data from a file
 def read_lcv(lcv_file):
 
-    dtype = np.dtype([('filter', 'S2'), ('mjd', float), ('mag', float),
+    dtype = np.dtype([('filter', 'U2'), ('mjd', float), ('mag', float),
         ('err', float)])
     data = np.loadtxt(lcv_file, dtype=dtype)
 
@@ -125,42 +150,25 @@ def refine_period(first_band, best_period, second_band=None,
 
 
 # Use to do one round of LombScargle and identify possible periods for RRL star
-def period_search(data, min_period = 0.2, max_period=1.0,
-                    error_threshold=0.05, verbose=0, plot=1):
+def period_search(data, min_period = 0.2, max_period=0.9,
+                    error_threshold=0.05, decimals=5, verbose=0, plot=1):
 
-    x1 = np.array(data['mjd'][data['err'] < error_threshold], dtype=float)
-    y1 = np.array(data['mag'][data['err'] < error_threshold], dtype=float)
-    er1 = np.array(data['err'][data['err'] < error_threshold], dtype=float)
+    x = np.array(data['mjd'][data['err'] < error_threshold], dtype=float)
+    y = np.array(data['mag'][data['err'] < error_threshold], dtype=float)
+    e = np.array(data['err'][data['err'] < error_threshold], dtype=float)
 
     freq_max = 1./(min_period)
     freq_min = 1./(max_period)
-    frequency, power = LombScargle(x1, y1, er1).autopower(minimum_frequency=freq_min,
-                        maximum_frequency=freq_max )
 
-    # Calculate noise level
-    median_power = np.median(power)
+    ls = LombScargle(x, y, e)
+    frequency, power = ls.autopower(minimum_frequency=freq_min,
+            maximum_frequency=freq_max)
+    fa = ls.false_alarm_probability(power.max())
 
-    # Find peaks
-    indices = peakutils.indexes(power, thres=0.5, min_dist=5000 )
-    candidate_periods = 1/frequency[indices]
     best_frequency = frequency[np.argmax(power)]
-    best_period = 1/best_frequency
+    period = 1/best_frequency
 
-    alias_freq = np.array([best_frequency+1, best_frequency-1, best_frequency+2, best_frequency-2])
-    alias_power = np.repeat(np.max(power), 4)
-
-
-
-    # Calculate SNR of peaks
-    snr = power[indices]/median_power
-    snr_best = power[np.argmax(power)]/median_power
-    if verbose == 1:
-        print candidate_periods
-        print snr
-    t_fit = np.linspace(0,1)
-    y_fit = LombScargle(x1, y1, er1).model(t_fit, best_frequency)
-
-    phase_data = np.mod(x1*best_frequency, 1)
+    phase_data = np.mod(x*best_frequency, 1)
 
     if plot == 1:
         fig = mp.figure(figsize=(12, 6))
@@ -168,20 +176,165 @@ def period_search(data, min_period = 0.2, max_period=1.0,
         ax2 = mp.subplot2grid((1,2), (0,1))
         ax1.plot(1/frequency, power)
         ax1.plot(1/best_frequency, np.max(power), 'rx')
+        ax1.text(0.05, 0.95, 'Best Period: {:.5f}'.format(period), transform=ax1.transAxes)
+        ax1.text(0.05, 0.9, 'False Alarm: {:.2e}'.format(fa), transform=ax1.transAxes)
         #ax1.plot(1/frequency[indices], power[indices], 'rx')
         #ax1.plot(1/alias_freq, alias_power, 'kx')
         ax1.set_xlim(min_period, max_period)
         ax1.set_xlabel('Period (days)')
         ax1.set_ylabel('Power')
 
-        ax2.plot(phase_data, y1, 'o')
-        ax2.set_ylim(np.max(y1)+0.05, np.min(y1)-0.05)
+        ax2.plot(phase_data, y, 'o')
+        ax2.set_ylim(np.max(y)+0.05, np.min(y)-0.05)
         ax2.set_xlabel('Phase')
         ax2.set_ylabel('mag')
         mp.show()
         mp.close()
 
-    return best_period, snr_best
+    period = np.round(period, decimals)
+
+    return period, fa
+
+def refine_period2(data, best_period,
+    error_threshold=0.05, plot=False):
+
+    x = np.array(data['mjd'][data['err'] < error_threshold], dtype=float)
+    y = np.array(data['mag'][data['err'] < error_threshold], dtype=float)
+    er = np.array(data['err'][data['err'] < error_threshold], dtype=float)
+
+    # Calculate required precision
+    delta_time = np.max(x) - np.min(x)
+    n_cycles = np.floor(delta_time/best_period)
+    max_precision = n_cycles * best_period / (n_cycles - 0.02) - best_period
+    order = np.ceil(np.abs(np.log10(max_precision)))
+    precision = 10**-order
+    print(precision)
+    delta_p = 0.00005
+    grid = np.linspace(best_period-delta_p, best_period+delta_p, num=1e-4/precision)
+
+    n_iters = len(grid)
+    avg_std = np.zeros(n_iters)
+    for i in range(n_iters):
+        p = grid[i]
+        phase = np.mod(x/p, 1)
+        stds, edges, bin_num = stats.binned_statistic(phase, y, statistic=np.std, bins=100)
+        counts, edges, bin_num = stats.binned_statistic(phase, y, statistic='count', bins=100)
+        avg_std[i] = np.mean(stds[counts > 3])
+
+
+    new_period = grid[np.argmin(avg_std)]
+    best_std = avg_std[np.argmin(avg_std)]
+    new_period = np.round(new_period, int(order))
+
+    if plot:
+        fig = mp.figure(figsize=(12, 6))
+        ax1 = mp.subplot2grid((1,2), (0,0))
+        ax2 = mp.subplot2grid((1,2), (0,1))
+        ax1.plot(grid, avg_std, 'ko')
+        ax1.axvline(new_period)
+        ax1.set_xlabel('period')
+        ax1.set_ylabel('average scatter')
+        phase = np.mod(x/new_period, 1)
+        ax2.plot(phase, y, 'o')
+        ax2.set_ylim(np.max(y)+0.05, np.min(y)-0.05)
+        ax2.text(0.05, 0.95, 'Best Period: {}'.format(new_period), transform=ax2.transAxes)
+        ax2.set_xlabel('Phase')
+        ax2.set_ylabel('mag')
+        mp.show()
+
+    return new_period
+
+
+def template_fit(lcv_data, min_period=0.2, max_period=0.9, decimals=5,
+    error_threshold=0.05, fix_period=0):
+
+    # read in data
+    x = np.array(lcv_data['mjd'][lcv_data['err'] < error_threshold], dtype=float)
+    y = np.array(lcv_data['mag'][lcv_data['err'] < error_threshold], dtype=float)
+    er = np.array(lcv_data['err'][lcv_data['err'] < error_threshold], dtype=float)
+
+    approx_amp = np.nanmax(y) - np.nanmin(y)
+    approx_epoch = x[np.argmin(y)]
+
+    n = int((max_period - min_period)/10**-decimals)
+    p_grid = np.linspace(min_period, max_period, num=n)
+
+    # read in template light curves
+    dir = '/Users/Jill/python/Light-Curve-Fitting/'
+    rrab = np.genfromtxt(dir+'rrab_optical_templates.dat')
+    rrc = np.genfromtxt(dir+'rrc_optical_templates.dat')
+
+
+    mag = y/approx_amp
+    shift = -0.5 - np.nanmin(mag)
+    mag += shift
+    temp_x = rrab[:,0]
+    temp_y = rrab[:,1]+0.5
+
+    if fix_period == 0:
+        std = np.zeros((n,7))
+        for i in range(n):
+            p = p_grid[i]
+            phase = np.mod((x-approx_epoch)/p, 1)
+
+            # interplotate on templates to find y_calc
+            for j in range(5):
+                rrab_temp_x = rrab[:,0]
+                rrab_temp_y = rrab[:,j+1]+0.5
+
+                y_calc = np.interp(phase, rrab_temp_x, rrab_temp_y)
+                std[i,j] = np.nanstd(mag-y_calc)
+            for j in range(2):
+                rrc_temp_x = rrc[:,0]
+                rrc_temp_y = rrc[:,j+1]
+
+                y_calc = np.interp(phase, rrc_temp_x, rrc_temp_y)
+                std[i,j+5] = np.nanstd(mag-y_calc)
+
+        mins = np.nanmin(std, axis=0)
+        best_temp = np.argmin(mins)
+        best_p = p_grid[np.argmin(std[:,best_temp])]
+        best_p = np.round(best_p, decimals)
+
+    else:
+        best_p = fix_period
+        phase = np.mod((x-approx_epoch)/best_p, 1)
+        std = np.zeros(7)
+        for j in range(5):
+            rrab_temp_x = rrab[:,0]
+            rrab_temp_y = rrab[:,j+1]+0.5
+
+            y_calc = np.interp(phase, rrab_temp_x, rrab_temp_y)
+            std[j] = np.nanstd(mag-y_calc)
+        for j in range(2):
+            rrc_temp_x = rrc[:,0]
+            rrc_temp_y = rrc[:,j+1]
+
+            y_calc = np.interp(phase, rrc_temp_x, rrc_temp_y)
+            std[j+5] = np.nanstd(mag-y_calc)
+
+        best_temp = np.argmin(std)
+
+    phase = np.mod((x-approx_epoch)/best_p, 1)
+    if best_temp <= 4:
+        temp_x = rrab[:,0]
+        temp_y = rrab[:,best_temp+1]+0.5
+    else:
+        temp_x = rrc[:,0]
+        temp_y = rrc[:,best_temp-4]+0.5
+
+    fig, ax = mp.subplots(1,1)
+    ax.errorbar(phase, mag, yerr=er, fmt='o')
+    ax.plot(temp_x, temp_y)
+    ax.text(0.05, 0.95, 'Template {}'.format(best_temp), transform=ax.transAxes)
+    ax.text(0.05, 0.90, 'Period = {}'.format(best_p), transform=ax.transAxes)
+    #ax.set_ylim(np.max(mag)+0.1, np.min(mag)-0.1)
+    ax.invert_yaxis()
+    mp.show()
+    mp.close()
+
+    return best_p
+
 
 # Derives a smoothed light curve using the GLOESS method. This version also
 # calculates the appropriate T0, and saves both the phased data and a
@@ -381,3 +534,51 @@ def gloess_auto(lcv_data, period, lcv_name, clean=1, save_dir='', master_plot=1)
 
 
     return lcv_params, T0
+
+# Use to do one round of LombScargle and identify possible periods for RRL star
+def LS_period(data, min_period = 0.2, max_period=1.0,
+                    error_threshold=0.05, precision='coarse', verbose=0, plot=1):
+
+    x1 = np.array(data['mjd'][data['err'] < error_threshold], dtype=float)
+    y1 = np.array(data['mag'][data['err'] < error_threshold], dtype=float)
+    er1 = np.array(data['err'][data['err'] < error_threshold], dtype=float)
+
+    freq_max = 1./(min_period)
+    freq_min = 1./(max_period)
+    delta_t = np.max(x1) - np.min(x1)
+    if precision == 'coarse':
+        delta_p = min_period - 1./(0.1/delta_t + 1/min_period)
+    if precision == 'fine':
+        delta_p = min_period - 1./(0.02/delta_t + 1/min_period)
+    order = np.floor(np.log10(delta_p))
+    precision_p = 10**order
+    precision_f = precision_p/min_period**2
+    frequency = np.arange(freq_min, freq_max, precision_f)
+    print(precision_p)
+    power = LombScargle(x1, y1, er1).power(frequency)
+
+    best_frequency = frequency[np.argmax(power)]
+    best_period = np.around(1./best_frequency,int(np.abs(order)))
+
+    phase_data = np.mod(x1*best_frequency, 1)
+
+    if plot == 1:
+        fig = mp.figure(figsize=(12, 6))
+        ax1 = mp.subplot2grid((1,2), (0,0))
+        ax2 = mp.subplot2grid((1,2), (0,1))
+        ax1.plot(1/frequency, power)
+        ax1.plot(1/best_frequency, np.max(power), 'rx')
+        #ax1.plot(1/frequency[indices], power[indices], 'rx')
+        #ax1.plot(1/alias_freq, alias_power, 'kx')
+        ax1.set_xlim(min_period, max_period)
+        ax1.set_xlabel('Period (days)')
+        ax1.set_ylabel('Power')
+
+        ax2.plot(phase_data, y1, 'o')
+        ax2.set_ylim(np.max(y1)+0.05, np.min(y1)-0.05)
+        ax2.set_xlabel('Phase')
+        ax2.set_ylabel('mag')
+        mp.show()
+        mp.close()
+
+    return best_period
